dc1dd370e60b81951c7efda5e6bfe3ccf2cf50be
Joshua <kurniawan.joshua21@gmail.com>
2023-06-30 08:58:12 +0800

    config next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 const path = require('path')
 
-const repo = 'porto'
-const basePath = `/${repo}`
-const assetPrefix = `/${repo}`
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+	// trim off `<owner>/`
+	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  
+	assetPrefix = `/${repo}/`
+	basePath = `/${repo}`
+  }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
