d510635a85b5c874b10268848a3d34c999ee802a
Joshua <kurniawan.joshua21@gmail.com>
2023-06-30 15:13:04 +0800

    edit

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 const path = require('path')
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = ''
+// let assetPrefix = ''
+// let basePath = ''
 
-if (isGithubActions) {
-	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+// if (isGithubActions) {
+// 	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-	assetPrefix = `/${repo}/`
-	basePath = `/${repo}`
-  }
+// 	assetPrefix = `/${repo}/`
+// 	basePath = `/${repo}`
+// }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	assetPrefix: assetPrefix,
-	basePath: basePath,
-	images: {loader: 'imgix',path: 'https://porto-8140.imgix.net'},
+	// assetPrefix: assetPrefix,
+	// basePath: basePath,
+	// images: {loader: 'imgix',path: 'https://porto-8140.imgix.net'},
 	output: 'export',
 	reactStrictMode: true,
 	experimental: {scrollRestoration: true},
