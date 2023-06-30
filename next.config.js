f22fb8d175a8ce7cb4068405a32feaa0ce8568b1
Joshua <kurniawan.joshua21@gmail.com>
2023-06-30 11:41:08 +0800

    adjust

--- next.config.js
+++ next.config.js
@@ -17,6 +17,7 @@ const nextConfig = {
 	assetPrefix: '/porto/',
 	basePath: basePath,
 	images: {
+		domains: 'https://porto-8140.imgix.net',
 		loader: 'imgix',
 		path: 'https://porto-8140.imgix.net'
 	},
