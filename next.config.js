a45b684d89f8f9360df13c81530e4a6607b21857
Joshua <kurniawan.joshua21@gmail.com>
2023-06-30 09:41:51 +0800

    adjust next config

--- next.config.js
+++ next.config.js
@@ -6,9 +6,8 @@ let assetPrefix = ''
 let basePath = '/'
 
 if (isGithubActions) {
-	// trim off `<owner>/`
 	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  
+
 	assetPrefix = `/${repo}/`
 	basePath = `/${repo}`
   }
