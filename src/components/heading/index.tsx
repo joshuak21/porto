3e5a20d68249e2d9bc5084446f4efcf61a4ef019
Joshua <kurniawan.joshua21@gmail.com>
2023-06-30 16:56:47 +0800

    heading favicon

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -18,7 +18,7 @@ export default function HeadingComponent({
 			<title>{props.title}</title>
 			<meta charSet="utf-8"/>
 
-			<link rel="icon" type="image/png" href="/favicon.ico" />
+			<link rel="icon" type="image/png" href="favicon.ico" />
 			<meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
 			<meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
