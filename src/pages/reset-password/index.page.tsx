ad649bd66f579ea701804a4add62c0e09d42737a
Joshua <kurniawan.joshua21@gmail.com>
2024-07-02 09:45:35 +0800

    Adjust button toggle password styling, add onchange and toggle password in reset password page

--- src/pages/reset-password/index.page.tsx
+++ src/pages/reset-password/index.page.tsx
@@ -7,6 +7,9 @@ import Styles from './style.module.css'
 
 class ElementData {
 	public isSubmitting: boolean = false
+	public data: { password: string, confirmPassword: string } = { password: '', confirmPassword: '' }
+	public confirmPassValidation: undefined | boolean = undefined
+	public toggleInput: { password: boolean, confirmPassword: boolean } = { password: false, confirmPassword: false }
 }
 
 export default function ResetPasswordPage(): JSX.Element {
@@ -23,6 +26,22 @@ export default function ResetPasswordPage(): JSX.Element {
 		forceUpdate()
 	}
 
+	function onChange(e: never): void {
+		const { name, value }: { name: string, value: string } = e['target']
+		d.data[name as keyof typeof d.data] = value
+		if (!!d.data.password && !!d.data.confirmPassword) {
+			d.confirmPassValidation = d.data.confirmPassword === d.data.password
+		} else {
+			d.confirmPassValidation = undefined
+		}
+		forceUpdate()
+	}
+
+	function toggleInput(type: 'password' | 'confirmPassword'): void {
+		d.toggleInput[type] = !d.toggleInput[type]
+		forceUpdate()
+	}
+
 	return (<>
 		<HeadingComponent title="Reset Password"/>
 		<div className="w-[100vw] h-[100vh] bg-sky-200 flex items-center justify-center p-4">
@@ -31,22 +50,22 @@ export default function ResetPasswordPage(): JSX.Element {
 				<form onSubmit={submitReset} className="pt-8 pb-4">
 					<div className="flex flex-col gap-4">
 						<div className={Styles['form-input']}>
-							<input type="password" id="new-password" name="new-password" placeholder="New Password" disabled={d.isSubmitting} className="text-[14px]"/>
-							<label htmlFor="new-password" className="text-[14px]">New Password</label>
-							<button type="button" disabled={d.isSubmitting} className={Styles['btn-toggle']}>
-								<i className="uil uil-eye"/>
+							<input type={d.toggleInput.password ? 'text' : 'password'} id="password" name="password" placeholder="New Password" disabled={d.isSubmitting} onChange={onChange} className="text-[14px]"/>
+							<label htmlFor="password" className="text-[14px]">New Password</label>
+							<button type="button" disabled={d.isSubmitting} onClick={(): void => toggleInput('password')} className={Styles['btn-toggle']}>
+								<i className={`uil uil-${d.toggleInput.password ? 'eye' : 'eye-slash'}`}/>
 							</button>
 						</div>
 						<div className={Styles['form-input']}>
-							<input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm Password" disabled={d.isSubmitting} className="text-[14px]"/>
+							<input type={d.toggleInput.confirmPassword ? 'text' : 'password'} id="confirm-password" name="confirmPassword" placeholder="Confirm Password" disabled={d.isSubmitting} onChange={onChange} className="text-[14px]"/>
 							<label htmlFor="confirm-password" className="text-[14px]">Confirm Password</label>
-							<button type="button" disabled={d.isSubmitting} className={Styles['btn-toggle']}>
-								<i className="uil uil-eye"/>
+							<button type="button" disabled={d.isSubmitting} onClick={(): void => toggleInput('confirmPassword')} className={Styles['btn-toggle']}>
+								<i className={`uil uil-${d.toggleInput.confirmPassword ? 'eye' : 'eye-slash'}`}/>
 							</button>
 						</div>
 					</div>
 					<div className="mt-8">
-						<button type="submit" className="w-full bg-sky-400 text-white rounded-[4px] py-1 hover:opacity-80 flex flex-row items-center justify-center disabled:opacity-50">
+						<button type="submit" disabled={!d.confirmPassValidation || d.isSubmitting} className="w-full bg-sky-400 text-white rounded-[4px] py-1 hover:opacity-80 flex flex-row items-center justify-center disabled:opacity-50">
 							{d.isSubmitting ? <i className="uil uil-spinner-alt leading-none icon-loader text-[18px]"/> : <span>Submit</span>}
 						</button>
 					</div>
